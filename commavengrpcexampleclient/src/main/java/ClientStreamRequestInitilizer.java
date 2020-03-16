292bf9bfdc32ce688c8af033f56e07116d2cef29
Arpan Solanki <arpan15apple@gmail.com>
2020-03-16 18:32:21 +0530

    Added Stream Proto file and Time-Latch

--- commavengrpcexampleclient/src/main/java/ClientStreamRequestInitilizer.java
+++ commavengrpcexampleclient/src/main/java/ClientStreamRequestInitilizer.java
@@ -6,43 +6,46 @@ import io.grpc.stub.StreamObserver;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 public class ClientStreamRequestInitilizer {
 
-public static void main(String[]args){
-    ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 8080).usePlaintext().build();
-   //for aysnc Stub use newStub  & for Sync stubs use future Stub
-    CommunicateStreamServiceGrpc.CommunicateStreamServiceStub asyncStub = CommunicateStreamServiceGrpc.newStub(managedChannel);
-     StreamObserver<GrpcStream.StreamDataRequest> requestObserver=asyncStub.communicateStreamServiceDynamic(new StreamObserver<GrpcStream.ClientStreamResponse>() {
-         @Override
-         public void onNext(GrpcStream.ClientStreamResponse clientStreamResponse) {
-             System.out.println("This is your Serserv Responce \n"+clientStreamResponse.getStreamDataResponse());
+    final static CountDownLatch finishedLatch = new CountDownLatch(1);
 
-         }
+    public static void main(String[] args) throws InterruptedException {
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 8080).usePlaintext().build();
+        //for aysnc Stub use newStub  & for Sync stubs use future Stub
+        CommunicateStreamServiceGrpc.CommunicateStreamServiceStub asyncStub = CommunicateStreamServiceGrpc.newStub(managedChannel);
+        StreamObserver<GrpcStream.StreamDataRequest> requestObserver = asyncStub.communicateStreamServiceDynamic(new StreamObserver<GrpcStream.ClientStreamResponse>() {
+            @Override
+            public void onNext(GrpcStream.ClientStreamResponse clientStreamResponse) {
+                System.out.println("This is your Serserv Responce \n" + clientStreamResponse.getStreamDataResponse());
+            }
 
-         @Override
-         public void onError(Throwable throwable) {
-             System.out.println("Exception Caught While Sending Tream Data With Error=="+throwable.toString());
-         }
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println("Exception Caught While Sending Tream Data With Error==" + throwable.toString());
+            }
 
-         @Override
-         public void onCompleted() {
-             System.out.println("Execution Completed ");
-         }
-     });
+            @Override
+            public void onCompleted() {
+                System.out.println("Execution Completed ");
+            }
+        });
 
-    processQueryList().parallelStream().forEach(x->{
-        requestObserver.onNext(GrpcStream.StreamDataRequest.newBuilder().setQuery(x).build());
-    });
+        processQueryList().parallelStream().forEach(x -> {
+            requestObserver.onNext(GrpcStream.StreamDataRequest.newBuilder().setQuery(x).build());
 
-}
-//returns a user defined query for processing from server side
-private static List<String>processQueryList(){
-    List<String>queryList=new ArrayList<String>();
-queryList.add("select *from employee");
-queryList.add("select *from employee where name ='shivankur'");
-queryList.add("select *from employee where id =2");
+        });
+        finishedLatch.await();
+    }
+
+    //returns a user defined query for processing from server side
+    private static List<String> processQueryList() {
+        List<String> queryList = new ArrayList<String>();
+        queryList.add("select *from ChotaAmazon");
+        queryList.add("select id1 from ChotaAmazon");
 //queryList.add("select * from user");
-    return queryList;
-}
+        return queryList;
+    }
 }
